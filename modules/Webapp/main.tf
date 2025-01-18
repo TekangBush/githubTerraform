f8574a1ad538599a1231b2d0569f7b8d3dff8a0d
Marccelus Enoh <menoh@pods.com>
2025-01-17 19:59:23 -0500

    referenced app plan correctly

--- modules/Webapp/main.tf
+++ modules/Webapp/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_app_service" "example" {
   name                = var.app_service_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  app_service_plan_id = azurerm_app_service_plan.appplan1.id
 
   site_config {
     dotnet_framework_version = "v4.0"
