c4a87489e66c3790bfa4c30b234ea95c9a109e39
Marccelus Enoh <menoh@pods.com>
2025-01-20 22:46:23 -0500

    fixed

--- modules/Webapp/main.tf
+++ modules/Webapp/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_app_service_plan" "appplan1" {
 
 resource "azurerm_app_service" "example" {
   name                = var.app_service_name
-  location            = azurerm_resource_group.example.locationss
+  location            = azurerm_resource_group.example.location
   resource_group_name     = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.appplan1.id
 
